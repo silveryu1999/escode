ffb620f023fb705f843d318fddf6dcb11fd3d053
silveryu1999 <931834214@qq.com>
2023-06-12 13:05:12 +0800

    reedsolomon

--- escode.py
+++ escode.py
@@ -66,14 +66,14 @@ for i in range(256):
         if multiply[i][j] == 1:
             inverse[i] = j
 
-# generate encode matrix (k+m)*k
+# generate encode matrix (k+m)*k using vandermonde matrix
 def gen_encode_matrix_vandermonde(k, m):
     # generate I
     I = np.zeros((k, k), dtype=int)
     for i in range(k):
         I[i][i] = 1
 
-    # generate vandermonde
+    # generate rows of vandermonde matrix
     vandermonde = np.zeros((m, k), dtype=int)
     for i in range(m):
         for j in range(k):
@@ -83,16 +83,47 @@ def gen_encode_matrix_vandermonde(k, m):
 
     return encode_matrix
 
+# generate encode matrix (k+m)*k using cauchy matrix
+def gen_encode_matrix_cauchy(k, m):
+    # generate I
+    I = np.zeros((k, k), dtype=int)
+    for i in range(k):
+        I[i][i] = 1
+
+    # generate rows of cauchy matrix
+    cauchy = np.zeros((m, k), dtype=int)
+    for i in range(m):
+        for j in range(k):
+            cauchy[i][j] = inverse[GF_add(i, j)]
+
+    encode_matrix = np.concatenate([I, cauchy])
+
+    return encode_matrix
+
 # generate decode matrix k*k according to missing rows
-def gen_decode_matrix(encode_matrix, missing_rows):
+def gen_decode_matrix_vandermonde(encode_matrix, missing_rows):
     # drop rows in encode matrix
     reconstruct_encode_matrix = np.delete(encode_matrix, missing_rows, axis=0)
     reconstruct_encode_matrix = reconstruct_encode_matrix[:encode_matrix.shape[1], :]
 
-    # get decode matrix
-    reconstruct_decode_matrix = get_inv_matrix(reconstruct_encode_matrix)
+    print("reconstruct_encode_matrix (vandermonde version):\n", reconstruct_encode_matrix)
 
-    return reconstruct_decode_matrix
+    # get decode matrix
+    flag, reconstruct_decode_matrix = get_inv_matrix(reconstruct_encode_matrix)
+
+    return flag, reconstruct_decode_matrix
+
+def gen_decode_matrix_cauchy(encode_matrix, missing_rows):
+    # drop rows in encode matrix
+    reconstruct_encode_matrix = np.delete(encode_matrix, missing_rows, axis=0)
+    reconstruct_encode_matrix = reconstruct_encode_matrix[:encode_matrix.shape[1], :]
+
+    print("reconstruct_encode_matrix (cauchy version):\n", reconstruct_encode_matrix)
+
+    # get decode matrix
+    flag, reconstruct_decode_matrix = get_inv_matrix(reconstruct_encode_matrix)
+
+    return flag, reconstruct_decode_matrix
 
 # calculate inv matrix
 def get_inv_matrix(matrix):
@@ -116,8 +147,8 @@ def get_inv_matrix(matrix):
                 j += 1
             
             if j == n:
-                # it's a singular matrix    
-                return None
+                # it's a singular matrix
+                return False, []
             
             # swap both A and inv
             A[[i, j], :] = A[[j, i], :]
@@ -145,7 +176,7 @@ def get_inv_matrix(matrix):
                     A[j][k] = GF_add(A[j][k], GF_multiply(v, A[i][k]))
                     inv[j][k] = GF_add(inv[j][k], GF_multiply(v, inv[i][k]))
 
-    return inv
+    return True, inv
 
 # matrix multiply in GF(2^8)
 # A is matrix, B is vector
@@ -172,27 +203,96 @@ def encode_data_drop(encode_data, m, drops):
 
     return incomplete_data
 
+# reedsolomon API
+def reedsolomon(k, m, raw_data, drops, matrix_type):
+    if matrix_type not in ["vandermonde", "cauchy"]:
+        print("matrix type error")
+        return False, []
+    
+    if k <= 0 or m <= 0:
+        print("k and m should not be 0")
+        return False, []
+    
+    if raw_data.shape[0] != k:
+        print("raw_data size != k")
+        return False, []
+    
+    if drops.shape[0] > m:
+        print("dropping too much sets of encoded data")
+        return False, []
+    
+    if drops.shape[0] != len(set(drops)):
+        print("dropping the duplicated sets of data")
+        return False, []
+    
+    if matrix_type == "vandermonde":
+        # using vandermonde version
+
+        # generate encode matrix using vandermonde matrix
+        em = gen_encode_matrix_vandermonde(k, m)
+        print("encode matrix (vandermonde version):\n", em)
+
+        # encode the raw data
+        encode_data = matrix_multiply(em, raw_data)
+        print("encoded data (vandermonde version):", encode_data)
+
+        # drop specified sets of the encoded data
+        incomplete_data = encode_data_drop(encode_data, m, drops)
+        print("data after lost (vandermonde version):", incomplete_data)
+
+        # generate decode matrix and reconstruct raw data (vandermonde)
+        flag, dm = gen_decode_matrix_vandermonde(em, drops)
+
+        if flag == False:
+            print("decode matrix (vandermonde version) is not invertible!")
+        else:
+            print("decode matrix (vandermonde version):\n", dm)
+            reconstruct_data = matrix_multiply(dm, incomplete_data)
+            print("reconstructed data (vandermonde version):", reconstruct_data)
+
+    else:
+        # using cauchy version
+
+        # generate encode matrix using cauchy matrix
+        em = gen_encode_matrix_cauchy(k, m)
+        print("encode matrix (cauchy version):\n", em)
+
+        # encode the raw data
+        encode_data = matrix_multiply(em, raw_data)
+        print("encoded data (cauchy version):", encode_data)
+
+        # drop specified sets of the encoded data
+        incomplete_data = encode_data_drop(encode_data, m, drops)
+        print("data after lost (cauchy version):", incomplete_data)
+
+        # generate decode matrix and reconstruct raw data (cauchy)
+        flag, dm = gen_decode_matrix_vandermonde(em, drops)
+
+        if flag == False:
+            print("decode matrix (cauchy version) is not invertible!")
+        else:
+            print("decode matrix (cauchy version):\n", dm)
+            reconstruct_data = matrix_multiply(dm, incomplete_data)
+            print("reconstructed data (cauchy version):", reconstruct_data)
+
+    return flag, dm
+
 
 if __name__ == '__main__':
+    # randomly choose k and m
     k = np.random.randint(3, 11)
     m = np.random.randint(1, k+1)
     print("k:", k, "m:", m)
 
+    # randomly generate raw data
     raw_data = np.array(np.random.randint(0, 256, k)).transpose()
     print("raw_data:", raw_data)
 
-    em = gen_encode_matrix_vandermonde(k, m)
-    print("encode matrix (vandermonde version):\n", em)
+    # randomly drop m sets of encoded data
+    drops = np.array(random.sample(range(0, k+m), m))
+    print("drops:", drops)
 
-    encode_data = matrix_multiply(em, raw_data)
-    print("encoded data:", encode_data)
-
-    drop = np.array(random.sample(range(0, k+m), m))
-    incomplete_data = encode_data_drop(encode_data, m, drop)
-    print("data after lost:", incomplete_data)
-
-    dm = gen_decode_matrix(em, drop)
-    print("decode matrix (vandermonde version):\n", dm)
-
-    reconstruct_data = matrix_multiply(dm, incomplete_data)
-    print("reconstructed data:", reconstruct_data)
+    # vandermonde
+    flag_v, re_data_v = reedsolomon(k, m, raw_data, drops, "vandermonde")
+    # cauchy
+    flag_c, re_data_c = reedsolomon(k, m, raw_data, drops, "cauchy")
